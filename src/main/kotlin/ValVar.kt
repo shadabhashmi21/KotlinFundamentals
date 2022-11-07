c511030a85b3f4a6fffe85803d3890169d76aea1
shadabhashmi21 <er.shadabhashmi@gmail.com>
2022-11-07 19:50:28 +0530

    feat: var/val

---
+++ src/main/kotlin/ValVar.kt
@@ -0,0 +1,11 @@
+fun main(){
+    // mutable variable
+    var text1 = "This variable can be reinitialized"
+    text1 = "Here, it is reinitialized"
+    println(text1)
+
+    // immutable variable
+    val text2 = "This variable cannot be reinitialized"
+    //text2 = "See Yourself, that's why this line is commented out"
+    println(text2)
+}
