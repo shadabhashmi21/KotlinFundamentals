a8be537b0388bf42f33247265b5080e4f6b63540
shadabhashmi21 <er.shadabhashmi@gmail.com>
2022-11-08 15:25:12 +0530

    feat: Basics

--- src/main/kotlin/Basics.kt
+++ src/main/kotlin/Basics.kt
@@ -8,4 +8,9 @@ fun main(){
     val text2 = "This variable cannot be reinitialized"
     //text2 = "See Yourself, that's why this line is commented out"
     println(text2)
+
+    /*In Kotlin, we don't have the charAt method for String
+    * Instead we just pass the index as demonstrated below*/
+    val str = "My String"
+    println(str[0])
 }
