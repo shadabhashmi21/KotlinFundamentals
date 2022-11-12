b12db35d5a6bb7f53940e1de9b397faf9a195940
shadabhashmi21 <er.shadabhashmi@gmail.com>
2022-11-12 21:21:32 +0530

    feat: JavaInteroperability

---
+++ src/main/kotlin/JavaInteroperability.java
@@ -0,0 +1,6 @@
+public class JavaInteroperability {
+    public static void main(String[] args) {
+        //The following method is being called from a Kotlin File
+        System.out.println(FunctionsKt.add(6,10));
+    }
+}
