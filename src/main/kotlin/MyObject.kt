b9e786750b773f5e80b76cbd4408112bb8c04989
shadabhashmi21 <er.shadabhashmi@gmail.com>
2022-11-16 12:08:39 +0530

    feat: Object

---
+++ src/main/kotlin/MyObject.kt
@@ -0,0 +1,32 @@
+fun main(){
+    CustomersData.count = 98
+    CustomersData.typeOfCustomers()
+    println(CustomersData.count)
+
+    println(CustomersData.typeOfCustomers())
+
+    CustomersData.count = 109
+    println(CustomersData.count)
+
+    CustomersData.myMethod("hello")
+}
+
+open class MySuperClass{
+    open fun myMethod(str: String) = println("MySuperClass myMethod called")
+}
+
+/*Whenever we have to use static values, we declare them in Object rather than Class.
+* Similar to static in Java*/
+object CustomersData: MySuperClass() {
+
+    var count: Int = -1             // Behaves like a STATIC variable
+
+    fun typeOfCustomers(): String { // Behaves like a STATIC method
+        return "Indian"
+    }
+
+    override fun myMethod(str: String) {    // Currently, behaving like a STATIC method
+        super.myMethod(str)
+        println("Object myMethod called")
+    }
+}
