4100e765d8037adf9a40fa93fc651d21595e5e5e
shadabhashmi21 <er.shadabhashmi@gmail.com>
2022-11-18 12:08:18 +0530

    completed: Lambda Function

--- src/main/kotlin/LambdaFunction.kt
+++ src/main/kotlin/LambdaFunction.kt
@@ -1,5 +1,6 @@
 fun main(){
     val program = Program()
+    var result = 0
 
     //Lambda expression
     val lambda: (Int, Int) -> Int = {x: Int, y: Int -> x + y} // The last expression "x + y" is the method body
@@ -8,12 +9,29 @@ fun main(){
     //Alternative approach, without using a variable
     program.add(10, 12) { x, y -> x + y }
     //program.add(10, 12, { x, y -> x + y }) // Outdated approach
+
+    // We are reassigning the outer variable "result" inside the Lambda expression
+    program.subtract(35, 10) {x,y -> result = x - y}
+    println(result)
+
+    /*When the lambda expression has only one parameter,
+    * we can use the "it" keyword instead of initialising a variable*/
+    program.reverseAndDisplay("Continue") {it.reversed()} // it.reversed() is the alternative of s -> s.reversed()
 }
 
 class Program{
     // High-level function with Lambda as parameter.
     fun add(a: Int, b: Int, action: (Int, Int) -> Int){
         val result = action(a, b)
+        println(result)
+    }
+
+    fun subtract(a: Int, b: Int, action: (Int, Int) -> Unit){
+        action(a, b)
+    }
+
+    fun reverseAndDisplay(str: String, reverse: (String) -> String){
+        val result = reverse(str)
         println(result)
     }
 }
